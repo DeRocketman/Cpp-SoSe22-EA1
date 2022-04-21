d1fc840dd13e9793f36f3875dc36aab47d29d548
stricker <de.rocketman@outlook.com>
2022-04-21 21:03:06 +0200

    ghf

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,30 +9,62 @@ class IceCreamParlor {
     string ice[3] = {"Capri", "Nogger", "Cornetto"};
     float price[3] = {1.10, 1.60, 2.30};
 
-    public:
-        void printIceCreamMenu() {
-            for (int i = 0; i < 3; ++i) {
+    void printIceCreamMenu() {
+        cout << "++++++++++++++++++ICE-CARD++++++++++++++++++" << endl;
+        for (int i = 0; i < 3; ++i) {
                 cout << ice[i] << " " << price[i] << "0 €" << endl;
-            }
         }
+        cout << "++++++++++++++++++++++++++++++++++++++++++++\n" << endl;
+    }
 
-        int printMainMenu() {
-            int userChoice = 4;
+    void printOrder() {
+        int userChoice = 5;
+        float credit = 0.0;
+        vector<string> order;
+        vector<float> bill;
 
-            cout << "Moin und herzlich Willkommen im IceShop" << endl;
+        cout << "" << endl;
+    }
+
+    void printRandomOrder() {
+
+    }
+
+public:
+
+    void printMainMenu() {
+        int userChoice = 4;
+
+        while (userChoice!=0) {
+            cout << "___---+++ICE++EIS++ICE++EIS+++ICE++EIS++ICE++EIS+++---___" << endl;
+            cout << "Moin und herzlich Willkommen im iceEshop" << endl;
             cout << "Bitte wählen Sie durch eingabe der Zahl:" << endl;
             cout << "[1]: Eiskarte anzeigen" << endl;
             cout << "[2]: Bestellung aufgeben" << endl;
-            cout << "[3]: Automatische Schnellwahl für mutige" << endl;
+            cout << "[3]: Automatische Schnellwahl für mutige Eisliebhaber :)" << endl;
             cout << "[0]: Ende" << endl;
+            cout << "________________________________________________________\n" << endl;
+            cout << "Ihre Auswahl: ";
             cin >> userChoice;
-            return userChoice;
+
+            if (userChoice > 3) {
+                cout << "Eingabe ist nicht gültig" << endl;
+                cout << "Weiter mit Eingabe" << endl;
+            } else if (userChoice == 1) {
+                printIceCreamMenu();
+            } else if (userChoice == 2) {
+                printOrder();
+            } else if (userChoice == 3) {
+                printRandomOrder();
+            } else {
+                cout << "Vielen Dank für ihren Besuch...Tschüss!" << endl;
+            }
         }
+    }
 };
 
 
 int main() {
     IceCreamParlor parlor;
     parlor.printMainMenu();
-
 }
